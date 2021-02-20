114af77eb07b64c6f3b31dbc58026d246b82bb82
Jonathan Bolanos <>
2021-02-20 13:19:00 -0500

    Init setup spring boot app, gitignore file

---
+++ src/main/java/com/appgate/ipgeolocation/IpgeolocationApplication.java
@@ -0,0 +1,13 @@
+package com.appgate.ipgeolocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpgeolocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpgeolocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appgate/ipgeolocation/IpgeolocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appgate.ipgeolocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpgeolocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
