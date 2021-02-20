4b13c4a5bc5de5d4b23d49b5edcb35a9faad6313
Jonathan Bolanos <>
2021-02-20 14:04:23 -0500

    Apply the application gradle plugin to add support for building a CLI application in Java

--- src/main/java/com/appgate/ipgeolocation/IpGeolocationApplication.java
+++ src/main/java/com/appgate/ipgeolocation/IpGeolocationApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IpgeolocationApplication {
+public class IpGeolocationApplication
+{
 
 	public static void main(String[] args) {
-		SpringApplication.run(IpgeolocationApplication.class, args);
+		SpringApplication.run(IpGeolocationApplication.class, args);
 	}
 
 }
